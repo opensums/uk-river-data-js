d277624de72d80a3ba6eb5cffdb9f2d37377b672
pbuk <pb@pbuk.uk>
2021-02-03 00:15:11 +0000

    test: function tests for Readings::getLatest()

--- test/functional/station-readings.test.js
+++ test/functional/station-readings.test.js
@@ -1,22 +1,37 @@
 import { createStationReadings } from '../..';
 
-describe('An object created by createCurrentReadings', () => {
-  describe('the latest readings for a station', async () => {
-    const readings = createStationReadings('3400TH');
-    const latest = await readings.getLatest();
+let latest;
 
-    it('should be more than 1m and 50cumecs', () => {
+async function getLatest(readings) {
+  if (!latest) {
+    latest = await readings.getLatest();
+  }
+  return latest;
+}
+
+describe('An object created by createCurrentReadings', () => {
+  describe('the latest readings for a station', () => {
+    const readings = createStationReadings('3400TH');
+
+    it('should be more than 1m and 50cumecs', async () => {
+      const latest = await getLatest(readings);
       expect(latest.level[1]).toBeGreaterThan(1);
       expect(latest.flow[1]).toBeGreaterThan(50);
       // Timestamps should be equal.
       expect(latest.flow[0]).toBe(latest.level[0]);
     });
 
-    it('should have equal timestamps within the last hour', () => {
+    it('should have equal timestamps within the last hour', async () => {
+      const latest = await getLatest(readings);
       expect(latest.flow[0]).toBe(latest.level[0]);
       const lag = Date.now() - new Date(latest.flow[0]).valueOf();
       expect(lag).toBeGreaterThan(0);
       expect(lag).toBeLessThan(3600000);
+    });
+
+    test('getLatest() should return a reference to readings.latest', async () => {
+      const latest = await getLatest(readings);
+      expect(latest).toBe(readings.latest);
     });
   });
 });
